099b8b8f19c91d244f5d982e72c55be825d72109
Jonathan Høj Hansen <jonathan2xh@gmail.com>
2023-03-13 09:57:37 +0100

    Opdaterede med loop

--- 1.py
+++ 1.py
@@ -1,1 +1,9 @@
-print("Mads er en klunk")
+import time
+
+i = 0
+print("Indlæser Mads' klunkethed:")
+while i < 101:
+  print("Mads er på " + str(i) + "% klunkethed")
+  i += 1
+  time.sleep(0.2)
+  
