65e20ab2d2520343f9ecef1897b95ecd949fe225
madskruse <mads-kruse@hotmail.com>
2023-03-13 10:03:11 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,9 +1,9 @@
 import time
 
 i = 0
-print("Indlæser Mads' klunkethed:")
+print("Indlæser Jonathan' klunkethed:")
 while i < 101:
-  print("Mads er på " + str(i) + "% klunkethed")
+  print("Jonathan er på " + str(i) + "% klunkethed")
   i += 1
   time.sleep(0.2)
   
